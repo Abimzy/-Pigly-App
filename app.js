91beb164eeb70f71d7a9e171656799b7ada386a2
Abimzy <eabiemo@gmail.com>
2019-03-26 23:25:58 -0700

    Remove unused test image and files

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ console.log("Hello");
 let subscribeButton = document.getElementById("mc-embedded-subscribe");
 
 subscribeButton.addEventListener("click", e => {
-  e.preventDefault();
+  // e.preventDefault();
   document.getElementById();
 });
 
 document.getElementById("mce-EMAIL").val();
 document.getElementById("mce-FNAME").val();
 document.getElementById("mce-LNAME").val();
+
+
+
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"deb1be34060c55b7aeb904bae","lid":"3fdb5aaa81","uniqueMethods":true}) })</script>
